af7d82a56d71ae981609e1e77372a134b814c4f8
Herz, Andreas <a.herz@freegroup.de>
2020-04-09 19:48:57 +0200

    remove deprecated documentation

--- docs/circuit/shapes/circuit/hardware/raspi/RaspiOUTPUT.md
+++
@@ -1,9 +0,0 @@
-# Raspberry Pi GPIO 
-
-## About GPIO
-The Raspberry Pi provides Input/Outport ports 
-intended to control or monitor other devices and 
-subsystem modules. These `GPIO` (general purpose I/O) 
-signals on the 2x13 header pins on the RPi motherboad  
-include SPI, I2C, serial UART, 3V3 and 5V power.
-
--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,6 @@ module.exports =[
   require('./webpack/circuit.config'),
   require('./webpack/designer.config'),
   require('./webpack/user.config'),
-  require('./webpack/demo-circuit.config'),
-  require('./webpack/demo-designer.config'),
   require('./webpack/login.config'),
   require('./webpack/resetpwd.config')
 ];
