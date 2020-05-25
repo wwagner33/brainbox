1fa8ab478e384e1977f73a3a7b4ef3d0e420b582
Herz, Andreas <a.herz@freegroup.de>
2020-05-25 23:26:06 +0200

    no message

--- app/stream.js
+++ app/stream.js
@@ -1,6 +1,6 @@
 var spawn = require('child_process').spawn;
 var http = require("http");
-var child = spawn('/opt/vc/bin/raspivid', ['-hf', '-w', '1280', '-h', '1024', '-t', '999999999', '-fps', '20', '-b', '5000000', '-o', '-']);
+var child = spawn('/opt/vc/bin/raspivid', ['-hf', '-w', '640', '-h', '480', '-t', '999999999', '-fps', '10', '-b', '5000000', '-o', '-']);
 var server = http.createServer(function(request, response) {
   child.stdout.pipe(response);
 });
