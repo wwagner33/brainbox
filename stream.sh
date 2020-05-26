43b4ab6f51a53181336079dd2ecb3cc5027d351c
Herz, Andreas <a.herz@freegroup.de>
2020-05-26 23:56:26 +0200

    no message

--- stream.sh
+++ stream.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-echo 'HTTPPort 8090
+echo '# ffserver configuration for an mjpeg stream
+# Adapted from
+# https://gist.github.com/peterhellberg/ebfc72147c2009ee720aafe57ce9c141
+HTTPPort 8080
 HTTPBindAddress 0.0.0.0
 MaxHTTPConnections 200
 MaxClients 100
@@ -7,23 +10,36 @@ MaxBandWidth 500000
 CustomLog -
 
 <Feed camera.ffm>
-   File /tmp/camera.ffm
-   FileMaxSize 200M
+File /tmp/camera.ffm
+FileMaxSize 5M
 </Feed>
 
-<Stream camera.mpeg>
+<Stream camera.mjpeg>
 Feed camera.ffm
-Format mpeg
-VideoFrameRate 25
-#VideoIntraOnly
+Format mpjpeg
+# Make sure frame rate and size
+# match those passed to ffmpeg
+VideoFrameRate 5
+VideoSize 640x480
+VideoGopSize 12
 VideoBitRate 4096
 VideoBufferSize 4096
-VideoSize hd720
 VideoQMin 5
 VideoQMax 51
-#NoAudio
+NoAudio
+Strict -1
+</Stream>
+
+<Stream static-camera.jpg>
+Feed camera.ffm
+Format jpeg
+VideoFrameRate 2
+VideoIntraOnly
+VideoSize 640x480
+NoAudio
+NoDefaults
 Strict -1
 </Stream>' > ${PWD}/.ffserver.conf
 (ffserver -f ${PWD}/.ffserver.conf > /dev/null)&
-(ffmpeg -s `1280x720 -f` v4l2 -i /dev/video0 -ac 1 -f alsa -i hw:2,0 http://localhost:8090/camera.ffm)&
+(ffmpeg -input_format mjpeg -video_size 640x480 -framerate 5 -i /dev/video0 -c:v copy http://localhost:8090/camera.ffm)&
 (rm ${PWD}/.ffserver.conf)&
